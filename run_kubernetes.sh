b1ce5dc7cfb30ce42aaeab426c9a7265215cbdad
Terry Jung <terry.jung88@gmail.com>
2020-12-07 17:31:06 -0500

    Initial commit

---
+++ run_kubernetes.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath="jungterry/mlapi"
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run mlapi\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=mlapi
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward mlapi 8000:80
